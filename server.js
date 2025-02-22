4175cff92a8773e4d870bc98374c78acc13006aa
Alen Koikkara <alendennis77@gmail.com>
2025-02-21 22:00:35 -0600

    added serverless

--- server.js
+++ server.js
@@ -10,8 +10,6 @@ const app = express()
 
 app.use(cors({ origin: ["http://localhost:3000", "https://aglet.vercel.app"] }))
 app.use(function(req, res, next) {
-  // res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
-  // res.header("Access-Control-Allow-Origin", "https://shoetopia-l55d.onrender.com"); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
